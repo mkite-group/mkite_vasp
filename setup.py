5669dca4a6576b889d81435ee264fec842b5ed6f
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2023-04-27 22:45:17 -0700

    setup and pyproject

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='mkite_vasp',
-    version='0.1.0',
-    packages=find_packages(include=['mkite_vasp']),
-)
-
+setup()
