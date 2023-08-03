4c9c0700af6b092939f83b0c45d7473af84780cd
dskoda <danielskoda@gmail.com>
2023-08-03 08:15:44 -0700

    fix bug with static VASP not saving jobresults.json in root folder

--- mkite_vasp/recipes/pbe/static.py
+++ mkite_vasp/recipes/pbe/static.py
@@ -1,6 +1,8 @@
+from mkite_core.recipes.pipes import SaveResultsPipe
+
 from mkite_vasp import VaspOptions, VaspSettings
 from mkite_vasp.parsers import VaspParser
-from mkite_vasp.recipes import VaspRecipe
+from mkite_vasp.recipes import VaspRecipe, VaspChain
 from mkite_vasp.runners import CustodianRunner
 
 
@@ -16,3 +18,10 @@ class StaticPBEVaspRecipe(VaspRecipe):
     OPTIONS_CLS = StaticPBEVaspOptions
     PARSER_CLS = VaspParser
     RUNNER_CLS = CustodianRunner
+
+
+class StaticPBEVaspChain(VaspChain):
+    JOBS = [
+        StaticPBEVaspRecipe,
+        SaveResultsPipe,
+    ]
--- mkite_vasp/recipes/rpbe/static.py
+++ mkite_vasp/recipes/rpbe/static.py
@@ -1,6 +1,8 @@
+from mkite_core.recipes.pipes import SaveResultsPipe
+
 from mkite_vasp import VaspOptions, VaspSettings
 from mkite_vasp.parsers import VaspParser
-from mkite_vasp.recipes import VaspRecipe
+from mkite_vasp.recipes import VaspRecipe, VaspChain
 from mkite_vasp.runners import CustodianRunner
 
 
@@ -17,3 +19,10 @@ class StaticRPBEVaspRecipe(VaspRecipe):
     OPTIONS_CLS = StaticRPBEVaspOptions
     PARSER_CLS = VaspParser
     RUNNER_CLS = CustodianRunner
+
+
+class StaticRPBEVaspChain(VaspChain):
+    JOBS = [
+        StaticRPBEVaspRecipe,
+        SaveResultsPipe,
+    ]
